e9ab77e6d53afed84018b0a66640551b84df8e35
ngothanhthien <mn.bf.thien@gmail.com>
2022-05-07 21:28:17 +0700

    Mã hóa password

--- user/process/login-process.php
+++ user/process/login-process.php
@@ -1,4 +1,8 @@
 <?php 
+function dang_nhap_that_bai(){
+    $_SESSION['message_lecture']='Sai tài khoản hoặc mật khẩu';
+    header('Location: ../layouts/login.php');
+}
 session_start();
 if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {        
     header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
@@ -7,17 +11,19 @@ if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERV
 require_once '../../root/database_connect.php';
 $account=mysqli_real_escape_string($mysqli,$_POST['account']);
 $password=mysqli_real_escape_string($mysqli,$_POST['password']);
-$sql = "SELECT id,name FROM lectures WHERE account = '$account' and password = '$password'";
-$result =$mysqli->query($sql);
+$result =$mysqli->query( "SELECT id,name,password FROM lectures WHERE account = '$account'");
 $count = mysqli_num_rows($result);
-$row = $result -> fetch_all(MYSQLI_ASSOC);
+if($count != 1) {
+    dang_nhap_that_bai();
+    require_once '../../root/database_close.php';
+}
+$row = $result -> fetch_all(MYSQLI_ASSOC)[0];
+$hash = $row['password'];
 require_once '../../root/database_close.php';
-if($count == 1) {
-    $_SESSION['lecture'] = $row[0]['id'];
-    $_SESSION['lecture_name']=$row[0]['name'];
+if (password_verify($password, $hash)){
+    $_SESSION['lecture'] = $row['id'];
+    $_SESSION['lecture_name']=$row['name'];
     header('Location: ../layouts/diemdanh.php');
- }else {
-    // $error = "Your Login Name or Password is invalid";
-    $_SESSION['message_lecture']='Sai tài khoản hoặc mật khẩu';
-    header('Location: ../layouts/login.php');
- }
+}
+dang_nhap_that_bai();
+
